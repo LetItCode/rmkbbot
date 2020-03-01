38e929344c9b45777c2c369874010fb0f96d6976
Josh Dies <LetItCode@users.noreply.github.com>
2020-03-01 22:48:02 +0300

    init

---
+++ module.js
@@ -0,0 +1,37 @@
+const Composer = require('telegraf/composer')
+const Markup = require('telegraf/markup')
+const bot = new Composer()
+
+bot.command(
+  ['start', 'help'],
+  Composer.branch(
+    ctx => {
+      const deeplink = ctx.message.text
+        .substring(7)
+        .replace(RegExp(`^${ctx.botInfo.username} `), '')
+      return deeplink === 'clear'
+    },
+    clear,
+    ({ botInfo, replyWithHTML }) =>
+      replyWithHTML(
+        "Hi! I can help you to clear chat from custom keyboard. Just invite me and everything's gonna be alrigh!",
+        Markup.inlineKeyboard([
+          Markup.urlButton(
+            'Invite dat cool bot',
+            `https://t.me/${botInfo.username}?startgroup=clear`
+          )
+        ]).extra()
+      )
+  )
+)
+
+bot.command('clear', clear)
+
+function clear ({ replyWithHTML }) {
+  replyWithHTML(
+    '🧽 Okay chat is cleared, you can send a BTC tip to: <code>1JosHdiEs32PfTAbbaVpuFXq3pG5bR5yBb</code>',
+    Markup.removeKeyboard().extra()
+  )
+}
+
+module.exports = bot
---
+++ readme.md
@@ -0,0 +1,12 @@
+# rmkbbot
+
+Telegram bot for remove custom keyboard from chat. Just invite him and everything's gonna be alrigh!
+
+[🤖Demo](https://t.me/rmkbbot)
+
+## Usage
+
+```sh
+npm install -g telegraf
+telegraf -t BOT_TOKEN module.js
+```
