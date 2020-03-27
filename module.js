2c954d6f889233cb483da4cca841ee6f82bc3075
Josh Dies <LetItCode@users.noreply.github.com>
2020-03-28 02:28:38 +0300

    cosmetic

--- module.js
+++ module.js
@@ -4,21 +4,13 @@ const bot = new Composer()
 bot.command(
   ['start', 'help'],
   Composer.branch(
-    ctx => {
-      const deeplink = ctx.message.text
-        .substring(7)
-        .replace(RegExp(`^${ctx.botInfo.username} `), '')
-      return deeplink === 'clear'
-    },
+    ctx => ctx.message.text.substring(7).replace(RegExp(`^${ctx.botInfo.username} `), '') === 'clear',
     clear,
     ({ botInfo, replyWithHTML }) =>
       replyWithHTML(
         "Hi! I can help you to clear chat from custom keyboard. Just invite me and everything's gonna be alrigh!",
         Markup.inlineKeyboard([
-          Markup.urlButton(
-            'Invite dat cool bot',
-            `https://t.me/${botInfo.username}?startgroup=clear`
-          )
+          Markup.urlButton('Invite dat cool bot', `https://t.me/${botInfo.username}?startgroup=clear`)
         ]).extra()
       )
   )
